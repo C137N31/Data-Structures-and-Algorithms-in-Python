b9008faf521a98c071e07ed5ec72beed570a14ab
C137N31 <C137N31@gmail.com>
2022-02-06 12:46:01 -0400

    Update R1.1.py

--- R1.1.py
+++ R1.1.py
@@ -1,2 +1,9 @@
-def is_multiple(n, m)
-    return True if n%m == 0 else False
+def is_multiple(n, m):
+    if m == 0: return False
+    return ( n%m == 0 )
+
+print (is_multiple(3,0))
+print (is_multiple(0,0))
+print (is_multiple(3,2))
+print (is_multiple(4,2))
+print (is_multiple(-4,2))
