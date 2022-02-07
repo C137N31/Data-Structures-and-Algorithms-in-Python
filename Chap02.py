29c4c2ee1bb51c79c48a0a1e661509764127aa8f
C137N31 <C137N31@gmail.com>
2022-02-07 14:49:34 -0400

    Update Chap02.py

--- Chap02.py
+++ Chap02.py
@@ -1,1 +1,19 @@
 #R2.1
+
+#auto-drive software
+#heartbeat monitor software
+#fall down detector
+
+#R2.2
+
+#a software which will adjust the produce based on selling markets
+
+#R2.3
+
+#search keyword
+#input keyword
+#compare keyword with every word in the opening file
+#highlight the matched word in the file
+
+#R2.4
+
