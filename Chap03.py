e6baaddadfa9379972cad954bf4d743b63e0e646
C137N31 <C137N31@gmail.com>
2022-02-09 10:25:17 -0400

    Update Chap03.py

--- Chap03.py
+++ Chap03.py
@@ -23,3 +23,26 @@ for y in ys:
 plt.yscale('log')
 plt.xscale('log')
 plt.show()         
+
+#R3.2
+
+import matplotlib.pyplot as plt
+import numpy
+import math
+
+x = [i/100 for i in range(1,1000)]
+
+funcs = [lambda x: 8*x*math.log(x),
+         lambda x: 2*x**2
+        ]
+
+ys = []
+
+for func in funcs:
+    ys.append(list(map(func, x)))
+
+plt.plot(x,ys[0],label='8*x*log(x)')
+plt.plot(x,ys[1],label='2*x**2')
+
+plt.legend()    
+plt.show() 
