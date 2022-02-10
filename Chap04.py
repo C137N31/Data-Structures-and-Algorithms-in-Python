af250926150f96b983132e4e937e871154b15a65
C137N31 <C137N31@gmail.com>
2022-02-09 22:52:32 -0400

    Update Chap04.py

--- Chap04.py
+++ Chap04.py
@@ -8,3 +8,26 @@ def maxi(S, n, m):
 S = [1,2,3,4,5,4,2]
 print(maxi(S,len(S),S[len(S)-1]))
 
+#R4.2
+
+def power(x, n):
+    if n == 0:
+        return 1
+    return x * power(x, n-1)
+
+print(power(2, 5))
+
+#R4.3
+
+def power(x, n):
+    if n == 1:
+        return x
+    partial = power(x, n//2)
+    result = partial * partial
+    if n%2 == 1:
+        result *= x
+    return result
+
+print(power(2, 18))
+
+#R4.4
